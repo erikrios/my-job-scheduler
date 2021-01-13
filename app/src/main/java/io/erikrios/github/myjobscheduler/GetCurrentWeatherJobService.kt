cb3618c11d3308d2f19cbf9e6eedf03b44ce15a2
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-13 09:37:27 +0700

    Create GetCurrentWeatherJobService.kt

---
+++ app/src/main/java/io/erikrios/github/myjobscheduler/GetCurrentWeatherJobService.kt
@@ -0,0 +1,15 @@
+package io.erikrios.github.myjobscheduler
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+
+class GetCurrentWeatherJobService : JobService() {
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        TODO("Not yet implemented")
+    }
+}
